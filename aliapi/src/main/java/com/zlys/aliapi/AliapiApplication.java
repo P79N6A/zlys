0ede2859c267c2e7405d5112cf7426f97276a01e
wq <521193913@qq.com>
2018-10-17 19:13:46 +0800

    Initial commit

---
+++ aliapi/src/main/java/com/zlys/aliapi/AliapiApplication.java
@@ -0,0 +1,12 @@
+package com.zlys.aliapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliapiApplication.class, args);
+    }
+}
